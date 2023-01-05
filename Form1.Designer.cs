a4c0a14509b735d5b51b534a3cad6d1d48a8a008
Shital <shitalshinde1205@gmail.com>
2023-01-05 10:24:32 +0530

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace Registration
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Username = new System.Windows.Forms.TextBox();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.Login = new System.Windows.Forms.Button();
+            this.Cancel = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Berlin Sans FB", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(109, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 27);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Berlin Sans FB", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(115, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(117, 27);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Password:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Username
+            // 
+            this.Username.Font = new System.Drawing.Font("Berlin Sans FB", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Username.Location = new System.Drawing.Point(277, 111);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(304, 35);
+            this.Username.TabIndex = 2;
+            this.Username.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Password
+            // 
+            this.Password.Font = new System.Drawing.Font("Berlin Sans FB", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Password.Location = new System.Drawing.Point(277, 168);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(304, 35);
+            this.Password.TabIndex = 3;
+            this.Password.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Password.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // Login
+            // 
+            this.Login.BackColor = System.Drawing.Color.SkyBlue;
+            this.Login.Font = new System.Drawing.Font("Berlin Sans FB", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Login.Location = new System.Drawing.Point(277, 246);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(104, 38);
+            this.Login.TabIndex = 4;
+            this.Login.Text = "Login";
+            this.Login.UseVisualStyleBackColor = false;
+            this.Login.Click += new System.EventHandler(this.Login_Click);
+            // 
+            // Cancel
+            // 
+            this.Cancel.BackColor = System.Drawing.Color.LightCoral;
+            this.Cancel.Font = new System.Drawing.Font("Berlin Sans FB", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Cancel.Location = new System.Drawing.Point(405, 246);
+            this.Cancel.Name = "Cancel";
+            this.Cancel.Size = new System.Drawing.Size(104, 38);
+            this.Cancel.TabIndex = 5;
+            this.Cancel.Text = "Cancel";
+            this.Cancel.UseVisualStyleBackColor = false;
+            this.Cancel.Click += new System.EventHandler(this.Cancel_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Berlin Sans FB", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(258, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(235, 33);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Registration Form";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Cancel);
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.Username);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Username;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.Button Login;
+        private System.Windows.Forms.Button Cancel;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Registration
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Password.PasswordChar = '*';
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+            if(Username.Text=="AbbVie" & Password.Text == "AbbVie")
+            {
+                MessageBox.Show("User Login Correct!","Welcome User!");
+                Second_Page page = new Second_Page();
+                page.Show();
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Sorry Incorrect User Login!","Warning");
+                Password.Text = "";
+                Username.Text = "";
+            }
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
